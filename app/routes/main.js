c67952298b395ba7afe182af115daec0dc8ede9a
snewcomer <snewcomer24@gmail.com>
2018-05-30 11:21:58 -0700

    another test case

--- app/routes/main.js
+++ app/routes/main.js
@@ -14,5 +14,6 @@ export default Route.extend({
     get(this, 'i18n');
     let i = this.get('i18n');
     let i = get(this, 'i18n');
+    let message = get(this, 'i18n').t('calendar.real-time-update', {});
   }
 });
